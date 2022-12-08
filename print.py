c39606874435c6cf6a6a7d89ce1515501c7a2149
Zylau <zylau@live.fr>
2022-12-08 11:28:17 +0100

    Unit Test helloName fail

--- print.py
+++ print.py
@@ -1,5 +1,9 @@
-def add1(x):
-    return x + 1
+def hello(name):
+    if name == "":
+        raise Exception("name is empty")
+    if len(name) > 30:
+        raise Exception("more than 30 characters")
+    return "Hello " + name
 
 
 print("Hello world")
--- test_sample.py
+++ test_sample.py
@@ -1,5 +1,18 @@
 # content of test_sample.py
+import pytest
 import print as print
 
-def test_answer():
-    assert print.add1(4) == 5
+def test_nameNotEmpty():
+    try:
+        print.hello("")
+    except Exception as e:
+        assert False, f"Nope {e}"
+
+def test_nameLengh():
+    try:
+        print.hello("azertyuiopqsdfghjklmwxcvbnazera")
+    except Exception as e:
+        assert False, f"Nope {e}"
+
+def test_hello():
+    assert print.hello("Tho") == "Hello Théo"
